daff90836fb1f3f918b8a1b5bd17ed3f63009029
Raziel <razzziel001@gmail.com>
2019-04-21 20:29:58 -0500

    conectamos a la base datos

---
+++ conexion.php
@@ -0,0 +1,5 @@
+<?php
+//Se utiliza para enlazar lo que hacemos a la base de datos
+$con = new mysqli('localhost','root','','cursophp'); //Nombrepag, NombreUser, Contraseña, NombreBsDs
+
+ ?>
---
+++ guardar.php
@@ -0,0 +1,6 @@
+<?php
+/*Se utiliza por si se realiza algún cambio en la conexion a la base de datos
+solo se modifica un archivo */
+include 'conexion.php';
+
+ ?>
